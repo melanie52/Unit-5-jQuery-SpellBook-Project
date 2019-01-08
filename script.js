34eaca8dbcb3e827acbc560e1dd0643f256cdb9a
melanie52 <43378785+melanie52@users.noreply.github.com>
2019-01-08 09:47:49 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,25 @@
+//Spell #1
+
+$("#lobster").click(function(){
+ $(".image1").toggle();
+ 
+});
+
+
+
+//Spell #2
+
+$("#CentralPerk").click(function(){
+ $(".image2").fadeToggle();
+});
+
+
+
+//Spell #3
+
+$("#howyoudoin").click(function(){
+ $(".image3").slideToggle();
+});
+
+
+
